08ec47ad2d3a65bd9ecf9490e86a4ece55b56a28
titoni-andrea <andreatitoni@gmail.com>
2024-06-13 11:30:27 +0200

    first commit

---
+++ dati.php
@@ -0,0 +1,53 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    for ($i = 0; $i < count($hotels); $i++) {
+        echo "<h2>" . $hotels[$i]['name'] . "</h2>";
+        echo "<p>" . $hotels[$i]['description'] . "</p>";
+        echo "<p>Parcheggio: " . $hotels[$i]['parking'] . "</p>";
+        echo "<p>Voto: " . $hotels[$i]['vote'] . "</p>";
+        echo "<p>Distanza dal centro: " . $hotels[$i]['distance_to_center'] . " km</p>";
+        echo "<hr>";
+    }
+
+?>
+
